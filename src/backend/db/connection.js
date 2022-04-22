604157f81a7c6fc67003494d94cea20220a341e8
thomastopuz <thomatop@gmail.com>
2022-04-22 20:41:25 +0200

    added mandatory dialect field and removed unnecessary packages

--- src/backend/db/connection.js
+++ src/backend/db/connection.js
@@ -1,2 +1,2 @@
 const {Sequelize} = require("sequelize");
-module.exports = new Sequelize(process.env.DB_CONNECTION_STRING);
+module.exports = new Sequelize(process.env.DB_CONNECTION_STRING, {dialect: "mysql"});
