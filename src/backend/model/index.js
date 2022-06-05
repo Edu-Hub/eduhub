58af52786bae1ccce2060b6f2e8ead5b3955805a
thomastopuz <thomatop@gmail.com>
2022-06-05 13:50:27 +0200

    added cascade deletion to relationships

--- src/backend/model/index.js
+++ src/backend/model/index.js
@@ -13,13 +13,13 @@ Group.belongsToMany(User, {through: "UserGroups"});
 Group.hasMany(Subject);
 Subject.belongsTo(Group);
 
-Subject.belongsTo(Directory, {as: "RootDirectory"});
+Subject.belongsTo(Directory, {as: "RootDirectory", onDelete: "CASCADE", hooks: true});
 Directory.hasOne(Subject);
 
-Directory.hasMany(Directory, {as: "Child"});
-Directory.hasOne(Directory, {as: "Parent"})
+Directory.hasMany(Directory, {as: "Child", onDelete: "CASCADE", hooks: true});
+Directory.hasOne(Directory, {as: "Parent"});
 
-Directory.hasMany(File);
+Directory.hasMany(File, {onDelete: "CASCADE", hooks: true});
 File.belongsTo(Directory);
 
 module.exports = {User, Group, Subject, Directory, File};
