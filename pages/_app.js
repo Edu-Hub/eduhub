7d83544a4894e299a35cc0e90e866ea9edf2b5c6
fanto6314 <antonio.foresta04@gmail.com>
2022-06-10 11:26:54 +0200

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
-import '../styles/globals.css'
-import {ChakraProvider} from '@chakra-ui/react';
-import {SessionProvider} from 'next-auth/react'
+import '../styles/globals.css';
+import {SessionProvider} from 'next-auth/react';
 
-function MyApp({Component, pageProps}) {
-    return (<ChakraProvider>
-        <SessionProvider session={pageProps.session} refetchInterval={0}>
-            <Component {...pageProps} />
-        </SessionProvider>
-    </ChakraProvider>);
+function MyApp({ Component, pageProps }) {
+  return (
+    <SessionProvider session={pageProps.session} refetchInterval={0}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
